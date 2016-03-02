7b067e06cbc1b785f0fed4db6c3d5d2d140a32bd
Daniel Kudwien <daniel@netzstrategen.com>
2016-03-02 13:41:43 +0000

    Initial commit of plugin core-standards 1.1.0.

---
+++ src/Admin.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @file
+ * Contains \Netzstrategen\CoreStandards\Admin.
+ */
+
+namespace Netzstrategen\CoreStandards;
+
+/**
+ * Administrative back-end functionality.
+ */
+class Admin {
+
+  /**
+   * @implements admin_init
+   */
+  public static function init() {
+  }
+
+}
---
+++ src/Plugin.php
@@ -0,0 +1,82 @@
+<?php
+
+/**
+ * @file
+ * Contains \Netzstrategen\CoreStandards\Plugin.
+ */
+
+namespace Netzstrategen\CoreStandards;
+
+/**
+ * Main front-end functionality.
+ */
+class Plugin {
+
+  /**
+   * Gettext localization domain.
+   *
+   * @var string
+   */
+  const L10N = 'core-standards';
+
+  /**
+   * @var string
+   */
+  private static $baseUrl;
+
+  /**
+   * @implements init
+   */
+  public static function init() {
+    // Add Facebook to oEmbed providers.
+    // @see https://developers.facebook.com/docs/plugins/oembed-endpoints
+    // @see https://developer.mozilla.org/en-US/Firefox/Privacy/Tracking_Protection
+    add_filter('oembed_providers', __CLASS__ . '::oembed_providers');
+
+    // Allow SVG files in media library.
+    add_filter('upload_mimes', __CLASS__ . '::upload_mime_types');
+
+    if (is_admin()) {
+      return;
+    }
+  }
+
+  /**
+   * @implements oembed_providers
+   */
+  public static function oembed_providers(array $providers) {
+    $providers['#https?://(www\.)?facebook\.com/(.+/)?video.+#i'] = ['https://www.facebook.com/plugins/video/oembed.{format}', TRUE];
+    $providers['#https?://(www\.)?facebook\.com/.+#i'] = ['https://www.facebook.com/plugins/post/oembed.{format}', TRUE];
+    return $providers;
+  }
+
+  /**
+   * @implements upload_mimes
+   */
+  public static function upload_mime_types(array $mimes) {
+    $mimes['svg'] = 'image/svg+xml';
+    return $mimes;
+  }
+
+  /**
+   * The base URL path to this plugin's folder.
+   *
+   * Uses plugins_url() instead of plugin_dir_url() to avoid a trailing slash.
+   */
+  public static function getBaseUrl() {
+    if (!isset(static::$baseUrl)) {
+      static::$baseUrl = plugins_url('', static::getBasePath() . '/custom.php');
+    }
+    return static::$baseUrl;
+  }
+
+  /**
+   * The absolute filesystem base path of this plugin.
+   *
+   * @return string
+   */
+  public static function getBasePath() {
+    return dirname(__DIR__);
+  }
+
+}
