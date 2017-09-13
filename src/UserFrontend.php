802c13dff7c02c62cd3d4c03287391055a9d16dd
Julià Mestieri <juli@netzstrategen.com>
2017-09-13 19:12:54 +0200

    Fixed user logged out when closing the web browser.

--- src/UserFrontend.php
+++ src/UserFrontend.php
@@ -60,8 +60,20 @@ class UserFrontend {
    * @implements login_form_defaults
    */
   public static function login_form_defaults(array $args) {
+    if (file_exists(get_stylesheet_directory() . '/dist/styles/login.css')) {
+      wp_enqueue_style('theme/login', get_stylesheet_directory_uri() . '/dist/styles/login.css');
+    }
+    else {
+      echo <<<EOD
+<style>
+#loginform .login-remember {
+  display: none;
+}
+</style
+
+EOD;
+    }
     $args['value_remember'] = TRUE;
-    $args['remember'] = FALSE;
     return $args;
   }
 
