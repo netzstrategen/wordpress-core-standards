03bc945e1bc7c919640768298ee77c444f6db960
Julià Mestieri <juli.quiron@gmail.com>
2019-05-29 09:40:05 +0200

    Update src/TrackingOptOut.php
    Co-Authored-By: Daniel Kudwien <daniel@netzstrategen.com>

--- src/TrackingOptOut.php
+++ src/TrackingOptOut.php
@@ -116,7 +116,7 @@ window.gaIdList = <?= json_encode($opt_out_ids) ?>;
    * @implements wp_enqueue_scripts.
    */
   public static function wp_enqueue_scripts() {
-    wp_enqueue_script(Plugin::PREFIX . '-enable-opt_out', Plugin::getBaseUrl() . '/dist/scripts/enable-opt-out.js', ['jquery'], TRUE);
+    wp_enqueue_script(Plugin::PREFIX . '-enable-opt_out', Plugin::getBaseUrl() . '/dist/scripts/enable-opt-out.js', ['jquery'], FALSE, TRUE);
     wp_localize_script(Plugin::PREFIX . '-enable-opt_out', 'core_standards_opt_out_toggle', ['confirmation_message' => __('Google Analytics tracking has been disabled.', Plugin::L10N)]);
   }
 
