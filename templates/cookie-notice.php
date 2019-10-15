5dd1835ae919c8a1b1aa9a5879276261f82dce3b
Fabian Marz <fabian@netzstrategen.com>
2019-10-15 17:07:52 +0200

    fixup! Added modal dialog to choose cookie groups triggering events to inject scripts only after consent.

--- templates/cookie-notice.php
+++ templates/cookie-notice.php
@@ -1,58 +1,100 @@
 <?php
 
 namespace Netzstrategen\CoreStandards;
+
+$options = [
+  [
+    'name' => 'basic',
+    'title' => __('Nur Butterkekse', Plugin::L10N),
+    'desc' => __('Erforderliche Funktionen', Plugin::L10N),
+    'atts' => [
+      'disabled' => TRUE,
+      'checked' => TRUE,
+    ],
+  ],
+  [
+    'name' => 'statistics',
+    'title' => __('Schokokekse', Plugin::L10N),
+    'desc' => __('Statistiken', Plugin::L10N),
+  ],
+  [
+    'name' => 'personalization',
+    'title' => __('Space-Cookies', Plugin::L10N),
+    'desc' => __('Personalisierung', Plugin::L10N),
+  ],
+];
+
+function create_attributes($atts) {
+  $attributes = [];
+  foreach ($atts as $name => $value) {
+    $attributes[] = $name . '="' . esc_attr($value) . '"';
+  }
+  return implode($attributes, ' ');
+}
 ?>
 
 <div class="cookie-notice" id="cookie-notice" role="banner">
   <div class="cookie-notice__body">
-    <h1>Kekse?</h1>
+    <h1><?= __('Kekse?', Plugin::L10N) ?></h1>
     <p><?= sprintf(__('Für den bestmöglichen Service bietet Ihnen diese Website Cookies. Mehr Infos finden Sie in unserer <a href="%s">Cookie-Bar</a>.', Plugin::L10N), apply_filters('core_standards/cookie_notice/policy_link', '/privacy')); ?></p>
-<div class="options">
-
-<div class="form-item form-checkbox">
-<input id="cookie-accept" type="checkbox" value="1" checked disabled />
-<label for="cookie-accept">Nur Butterkekse
-<br><small>Erforderliche Funktionen</small>
-</label>
-</div>
-<div class="form-item form-checkbox">
-<input id="cookie-accept" type="checkbox" value="1" />
-<label for="cookie-accept">Schokokekse
-<br><small>Statistiken</small>
-</label>
-</div>
-<div class="form-item form-checkbox">
-<input id="cookie-accept" type="checkbox" value="1" />
-<label for="cookie-accept">Space-Cookies
-<br><small>Personalisierung</small>
-</label>
-</div>
-
-</div>
-
-<div class="form-actions">
-<button class="button btn">Meine Auswahl bestätigen</button>
-<button class="button button--primary btn btn--primary">Gönn ich mir alle</button>
-</div>
-
+    <?php if ($options): ?>
+      <div class="options">
+        <?php foreach ($options as $option): ?>
+          <div class="form-item form-checkbox">
+            <label for="<?= esc_attr(sanitize_title($option['name'])) ?>">
+              <input id="<?= esc_attr(sanitize_title($option['name'])) ?>" name="cookies" type="checkbox" value="<?= esc_attr($option['name']) ?>" <?= create_attributes($option['atts'] ?? []); ?> />
+              <?= esc_html($option['title']) ?>
+              <br><small><?= esc_html($option['desc']) ?></small>
+            </label>
+          </div>
+        <?php endforeach; ?>
+      </div>
+    <?php endif; ?>
+    <div class="form-actions">
+      <button class="button btn" data-js="confirm"><?= __('Meine Auswahl bestätigen', Plugin::L10N) ?></button>
+      <button class="button button--primary btn btn--primary" data-js="confirm-all"><?= __('Gönn ich mir alle', Plugin::L10N) ?></button>
+    </div>
   </div>
-  <!--a class="cookie-notice__close" href="#" id="cookies-accept" aria-label="<?= __('OK', Plugin::L10N) ?>"></a-->
 </div>
 
 <script>
 (function () {
-  if (typeof window.localStorage !== 'undefined') {
-    var cookie_notice = document.getElementById('cookie-notice');
-    if (window.localStorage.getItem('cookies-accepted')) {
-      cookie_notice.setAttribute('hidden', 'true');
-      return false;
-    }
-    document.getElementById('cookies-accept').addEventListener('click', function (e) {
-      e.preventDefault();
-      e.stopPropagation();
-      window.localStorage.setItem('cookies-accepted', 'true');
-      cookie_notice.setAttribute('hidden', 'true');
+  if (typeof window.localStorage === 'undefined') {
+    return;
+  }
+
+  function hideCookieNotice() {
+    const cookie_notice = document.getElementById('cookie-notice');
+    cookie_notice.setAttribute('hidden', 'true');
+    document.body.classList.remove('has-cookie-notice');
+  }
+
+  function DOMContentLoaded() {
+    document.body.classList.add('has-cookie-notice');
+    document.addEventListener('click', (event) => {
+      if (event.target.dataset.js !== 'confirm' && event.target.dataset.js !== 'confirm-all') {
+        return;
+      }
+      const values = [];
+      const checkboxes = document.querySelectorAll('input[name=cookies]');
+      for (checkbox of checkboxes) {
+        if (event.target.dataset.js === 'confirm-all') {
+          checkbox.checked = true;
+        }
+        if (checkbox.checked) {
+          values.push(checkbox.value);
+        }
+      }
+      window.localStorage.setItem('cookies-accepted', JSON.stringify(values));
+      hideCookieNotice();
     });
   }
+
+  if (JSON.parse(window.localStorage.getItem('cookies-accepted'))) {
+    hideCookieNotice();
+    return false;
+  }
+
+  document.addEventListener('DOMContentLoaded', DOMContentLoaded);
 })();
 </script>
