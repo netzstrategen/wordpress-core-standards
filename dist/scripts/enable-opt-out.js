033c15eff0c5dcc67b6d47e49830fac7a61c4941
Fabian Marz <fabian@netzstrategen.com>
2019-10-16 16:34:21 +0200

    Added gulp-task-collection and linters.

--- dist/scripts/enable-opt-out.js
+++ dist/scripts/enable-opt-out.js
@@ -1,7 +1,10 @@
-"use strict";
+'use strict';
 
-!function(t) {
-    t(".core-standards-opt-out-link").on("click", function(t) {
-        t.preventDefault(), gaOptout(), alert(core_standards_opt_out_toggle.confirmation_message);
-    });
-}(jQuery);
+(function ($) {
+  // Triggers out-out cookie creation when click on link.
+  $('.core-standards-opt-out-link').on('click', function (e) {
+    e.preventDefault();
+    gaOptout();
+    alert(core_standards_opt_out_toggle.confirmation_message);
+  });
+})(jQuery);
