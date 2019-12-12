2d51fc0827cb1ee851d776d3f8748bc4dd30b209
Evelia Molina <evelia@netzstrategen.com>
2019-12-12 11:49:26 +0100

    fixup! Bumped Plugin version.

--- assets/scripts/cookie-consent.js
+++ assets/scripts/cookie-consent.js
@@ -35,7 +35,7 @@
         data.consent[checkbox.value] = checkbox.checked;
       }
       window.localStorage.setItem('cookie-consent', JSON.stringify(data));
-      $.post(window.core_standards.ajaxurl, {
+      $.post(window.core_standards.ajaxurl_cookie_consent, {
         action: 'core-standards/log_cookie_consent',
         consent: data,
         referer: window.location.pathname,
