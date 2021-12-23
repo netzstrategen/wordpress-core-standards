ae7b7b9cfb64224b69ef04bf55372681efaea066
Christian <christian@netzstrategen.com>
2021-12-23 12:25:05 -0600

    Changed Content Security Policy to frame-ancestors

--- src/Security.php
+++ src/Security.php
@@ -66,7 +66,7 @@ class Security {
       $headers['X-Content-Type-Options']    ??= defined('CORE_STANDARDS_X_Content_Type_Options') ? CORE_STANDARDS_X_Content_Type_Options : 'nosniff';
       $headers['X-XSS-Protection']          ??= defined('CORE_STANDARDS_X_XSS_Protection') ? CORE_STANDARDS_X_XSS_Protection : '1; mode=block';
       $headers['Permissions-Policy']        ??= defined('CORE_STANDARDS_Permissions_Policy') ? CORE_STANDARDS_Permissions_Policy : 'geolocation=(self "https://example.com") microphone=() camera=()';
-      $headers['Content-Security-Policy']   ??= defined('CORE_STANDARDS_Content_Security_Policy') ? CORE_STANDARDS_Content_Security_Policy : 'script-src "self"';
+      $headers['Content-Security-Policy']   ??= defined('CORE_STANDARDS_Content_Security_Policy') ? CORE_STANDARDS_Content_Security_Policy : 'frame-ancestors';
       $headers['X-Frame-Options']           ??= defined('CORE_STANDARDS_X_Frame_Options') ? CORE_STANDARDS_X_Frame_Options : 'SAMEORIGIN';
       $headers['Strict-Transport-Security'] ??= defined('CORE_STANDARDS_Strict_Transport_Security') ? CORE_STANDARDS_Strict_Transport_Security : 'Strict-Transport-Security: max-age=31536000; includeSubDomains';
     }
