0ec5cb01c3a7eb4435d86c82c544d20530659190
Fabian Marz <fabian@netzstrategen.com>
2023-02-21 11:51:26 +0100

    Added multilingual support for WP CLI.
    Currently only supports WPML.

---
+++ src/Hooks/MultilingualSupport.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @file
+ * Contains \Netzstrategen\CoreStandards\Hooks\MultilingualSupport.
+ */
+
+namespace Netzstrategen\CoreStandards\Hooks;
+
+class MultilingualSupport {
+
+  /**
+   * Changes the command language to return entities of matching language only.
+   *
+   * @param array $args
+   * @param array $assoc_args
+   * @param array $options
+   *
+   * @return array
+   */
+  public static function changeLanguage(array $args, array $assoc_args, array $options): array {
+    global $sitepress;
+    if ($sitepress && $lang = $assoc_args['lang'] ?? '') {
+      $sitepress->switch_lang($lang, TRUE);
+    }
+    return $args;
+  }
+}
